70bb0e10c874ee2a91605f9f9b440134a70f32ca
ferdynand ergy pramudani <ferdynandergypramudani11@gmail.com>
2024-09-29 08:28:59 +0700

    create component

---
+++ src/MenuResto.jsx
@@ -0,0 +1,18 @@
+import "./MenuResto.css";
+
+function MenuResto() {
+  return (
+    <div className="menuResto">
+      <div>
+        <b>Nama Menu:</b>
+        <em>Nasi Uduk 🍚</em>
+      </div>
+      <div>
+        <b>Harga:</b>
+        <em>Rp 20.000</em>
+      </div>
+    </div>
+  );
+}
+
+export default MenuResto;
